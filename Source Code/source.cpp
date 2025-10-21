ace773f8eed0a33809e4f5b85d30cb4c474ecaf0
Ali-Rehan <alirehan2152@gmail.com>
2025-10-22 02:25:55 +0500

    Added the function for a greeting msg and generation of weather, obstacle and malfunction

--- Source Code/source.cpp
+++ Source Code/source.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+inline void startingmsg();
+inline int weathergen();
+inline bool obstacle();
+inline bool malfunction();
 
 void main() {
-    
+    srand(time(0));
 
-    
+    startingmsg();
+
+    std::cout << "---Delivery to location A---\n";
+
+
+
+}
+
+inline void startingmsg() {
+    std::cout << "---------------Delivery Drone---------------" << std::endl;
+    std::cout << "Good Morning!" << std::endl;
+    std::cout << "Let's start the day. \n\n" << std::endl;
+}
+
+inline int weathergen() {
+    return rand() % 6 + 1;
+}
+
+inline bool obstacle() {
+    return rand() % 2;
+}
+
+inline bool malfunction() {
+    if (rand() % 10 == 7) {
+        return 1;
+    }
+    return 0;
 }
